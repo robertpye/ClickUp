5111db7fe0b2d7f4b5abe62c8baea9e8c1e57126
Rob Pye <robert.pye@ethosvo.org>
2023-04-18 17:59:46 +0100

    modified:   CU Dupes.py This commit adds a page break to ensure that the clickup API does not get stuck after reading 100 tasks

--- CU Dupes.py
+++ CU Dupes.py
@@ -1,67 +1,64 @@
-from secrets import api_key, team_id, list_id
 import requests
+import time
+from secrets import api_key, team_id, list_id
 
-def get_filtered_tasks(team_id):
-    url = f'https://api.clickup.com/api/v2/team/{team_id}/task'
-    headers = {
-        'Authorization': api_key
-    }
-    params = {
-        'list_ids[]': list_id,
-        'page': 0
-    }
+headers = {
+    "Authorization": api_key,
+    "Content-Type": "application/json"
+}
+
+api_calls = 0
+
+def pause_if_needed():
+    global api_calls
+    if api_calls >= 98:
+        print("Pausing due to max API calls.")
+        time.sleep(65)
+        api_calls = 0
+
+def get_all_tasks():
     all_tasks = []
-    
+    page = 0
     while True:
+        url = f"https://api.clickup.com/api/v2/team/{team_id}/task"
+        params = {
+            "list_ids[]": list_id,
+            "page": page
+        }
         response = requests.get(url, headers=headers, params=params)
-        
-        if response.status_code != 200:
-            raise Exception(f'Error fetching tasks: {response.text}')
-        
-        tasks = response.json().get('tasks', [])
-        all_tasks.extend(tasks)
-        
-        if len(tasks) < 100:
+        response.raise_for_status()
+        tasks = response.json()["tasks"]
+        if not tasks:
             break
-        
-        params['page'] += 1
-    
+        all_tasks.extend(tasks)
+        page += 1
     return all_tasks
 
-def find_duplicate_tasks(tasks):
-    task_titles = {}
+
+def add_tag_to_task(task_id, tag_name):
+    global api_calls
+    url = f"https://api.clickup.com/api/v2/task/{task_id}/tag/{tag_name}"
+    response = requests.post(url, headers=headers)
+    response.raise_for_status()
+    api_calls += 1
+    pause_if_needed()
+
+def find_and_tag_duplicate_tasks(tasks):
+    task_names = {}
     duplicate_tasks = []
-    
     for task in tasks:
-        if task['name'] in task_titles:
-            duplicate_tasks.append(task)
-            duplicate_tasks.extend(task_titles[task['name']])
-            task_titles[task['name']].append(task)
+        task_name = task["name"]
+        if task_name in task_names:
+            duplicate_task_id = task["id"]
+            print(f"Found duplicate: Task ID - {duplicate_task_id}, Task name - {task_name}")
+            add_tag_to_task(duplicate_task_id, "possible_duplicate")
+            duplicate_tasks.append(duplicate_task_id)
         else:
-            task_titles[task['name']] = [task]
-    
+            task_names[task_name] = task["id"]
     return duplicate_tasks
 
-def add_tag_to_task(task_id, task_name, tag_name):
-    url = f'https://api.clickup.com/api/v2/task/{task_id}/tag/{tag_name}'
-    headers = {
-        'Authorization': api_key,
-        'Content-Type': 'application/json'
-    }
-    response = requests.post(url, headers=headers)
-    
-    if response.status_code != 200:
-        raise Exception(f'Error adding tag to task: {response.text}')
-    
-    print(f'Added "{tag_name}" tag to task "{task_name}" with ID {task_id}')
+tasks = get_all_tasks()
+print(f"Total number of tasks: {len(tasks)}")
 
-if __name__ == '__main__':
-    tasks = get_filtered_tasks(team_id)
-    print(f'Total number of tasks: {len(tasks)}')
-    
-    duplicate_tasks = find_duplicate_tasks(tasks)
-    print(f'Number of duplicate tasks: {len(duplicate_tasks)}')
-    
-    unique_duplicate_tasks = {task['id']: task for task in duplicate_tasks}.values()
-    for task in unique_duplicate_tasks:
-        add_tag_to_task(task['id'], task['name'], 'possible_duplicate')
+duplicate_tasks = find_and_tag_duplicate_tasks(tasks)
+print(f"Total number of duplicate tasks: {len(duplicate_tasks)}")
